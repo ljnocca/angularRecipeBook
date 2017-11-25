b37d390f50c7f683ff5473b3869f80db03f8e5a2
Laurent Nocca <lnocca@fanreact.com>
2017-11-24 18:32:12 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBlXOgawltJ0JWeIiFlYLGzJyAkUbOSgjg",
+      authDomain: "angularrecipeproject-5787b.firebaseapp.com"
+    });
+  }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,21 +3,26 @@ import {Http, Response} from '@angular/http';
 import {RecipeService} from '../recipes/recipe.service';
 import {Recipe} from '../recipes/recipe.model';
 import 'rxjs/Rx';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
   constructor(private http: Http,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
     // use put to OVERWRITE the database
     // add .json to avoid CORS errors
-    return this.http.put('https://recipeangularproj.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put('https://angularrecipeproject-5787b.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    return this.http.get('https://recipeangularproj.firebaseio.com/recipes.json')
+    const token = this.authService.getToken();
+
+    return this.http.get('https://angularrecipeproject-5787b.firebaseio.com/recipes.json?auth=' + token)
       .map(
         (response: Response) => {
           const recipes: Recipe[] = response.json();
