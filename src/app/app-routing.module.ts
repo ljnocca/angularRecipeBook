7b64dc0b7691634b74ceca58f0a8bd7f235e0990
Laurent Nocca <lnocca@fanreact.com>
2017-11-26 16:44:57 -0600

    added modules and optimized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,30 +1,17 @@
 import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes, PreloadAllModules} from '@angular/router';
 
-import {RecipesComponent} from './recipes/recipes.component'
 import {ShoppingListComponent} from './shopping-list/shopping-list.component'
-import {RecipeStartComponent} from './recipes/recipe-start/recipe-start.component'
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component'
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component'
-import {SignupComponent} from './auth/signup/signup.component'
-import {SigninComponent} from './auth/signin/signin.component'
-import {AuthGuardService} from './auth/auth-guard.service';
+import {HomeComponent} from './Core/home/home.component'
 
 const appRoutes: Routes = [
-  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
-  {path: 'recipes', component: RecipesComponent, children: [
-    {path: '', component: RecipeStartComponent},
-    {path: 'new', component: RecipeEditComponent, canActivate: [AuthGuardService]},
-    {path: ':id', component: RecipeDetailComponent},
-    {path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuardService]},
-  ]},
-  {path: 'shopping-list', component: ShoppingListComponent},
-  {path: 'signup', component: SignupComponent},
-  {path: 'signin', component: SigninComponent},
+  {path: '', component: HomeComponent},
+  {path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
+  {path: 'shopping-list', component: ShoppingListComponent}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
